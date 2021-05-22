2e738232830bb4a401eab1edc1fd0220c15bdbd6
Peter Diener <peter@pollak-diener.de>
2021-05-23 00:32:18 +0200

    Fixed hexparser bug.

--- src/intelhexparser.cpp
+++ src/intelhexparser.cpp
@@ -16,7 +16,7 @@ bool IntelHexParser::parse(QString file)
 
     quint16 extendetSegmentAddress = 0;
 
-    quint64 extendetLinearAddress = 0;
+    quint64 extendedLinearAddress = 0;
 
     if(file.isEmpty())
         return false;
@@ -27,6 +27,9 @@ bool IntelHexParser::parse(QString file)
         fprintf(stdout, "Unable to open File: %s\n", hexFile.errorString().toLocal8Bit().append("\0").data());
         return false;
     }
+
+    program.clear();
+    program.fill(0xff, 256000);
 
     qint64 currentLine = 1;
     bool endOfFile = false;
@@ -41,15 +44,20 @@ bool IntelHexParser::parse(QString file)
         parsedLine = parseLine(buffer);
 
         if(!parsedLine.valid){  // abort parsing when a line gets marked as not valid
-            fprintf(stdout, "line %ll not valid\n"
-                            "aborting parse\n", &currentLine);
+            fprintf(stdout, "line %lli not valid\n"
+                            "aborting parse\n", currentLine);
             hexFile.close();
             return false;
         }
 
         switch (parsedLine.entyType) {
         case DATA_RECORD:
-            program.insert(extendetLinearAddress + parsedLine.address + extendetSegmentAddress * 16, parsedLine.data);
+//            program.insert(extendedLinearAddress + parsedLine.address + extendetSegmentAddress * 16, parsedLine.data);
+            quint32 adr;
+            adr = (quint32)extendetSegmentAddress * 16 + (quint32)extendedLinearAddress + (quint32)parsedLine.address;
+            for (quint8 i=0; i < parsedLine.bytecount; i++)
+                program[adr + i] = parsedLine.data.at(i);
+
             break;
         case END_OF_FILE:
             endOfFile = true;
@@ -64,15 +72,15 @@ bool IntelHexParser::parse(QString file)
             break;
         case EXTENDED_LINEAR_ADDRESS_RECORD:
             qDebug() << "Extendet Linear Address Record";
-            extendetLinearAddress = (parsedLine.data.toLongLong() << 16);
+            extendedLinearAddress = (parsedLine.data.toLongLong() << 16);
             break;
         case START_LINEAR_ADDRESS_RECORD:
             qDebug() << "Start Linear Address Record";
             return false;
             break;
         default:
-            fprintf(stdout, "unrecognised entry type at line %ll\n"
-                            "aborting parse\n", &currentLine);
+            fprintf(stdout, "unrecognised entry type at line %lli\n"
+                            "aborting parse\n", currentLine);
             hexFile.close();
             return false;
 
@@ -80,6 +88,9 @@ bool IntelHexParser::parse(QString file)
 
         currentLine++;
     }
+
+    while ((quint8)program.back() == 0xff)
+        program.chop(1);
 
     hexFile.close();
 
@@ -106,7 +117,7 @@ IntelHexParser::intelHexLine IntelHexParser::parseLine(QString lineString)
     QByteArray lineByteArray;
 
     // lines must start with a colon
-    if (lineString[0] != ":"){
+    if ((lineString.length() < 1) || (lineString[0] != ":")){
         fprintf(stdout, "IntelHexParser: parseLine failed: no valid start character\n");
         parsedLine.valid = false;
         return parsedLine;
@@ -117,17 +128,18 @@ IntelHexParser::intelHexLine IntelHexParser::parseLine(QString lineString)
     lineByteArray = QByteArray::fromHex(lineString.toLocal8Bit());
 
     // line must not be empty
-    if (lineByteArray.isEmpty()){
+    if (lineByteArray.length() < 5){
         fprintf(stdout, "IntelHexParser: parseLine failed: line empty\n");
         parsedLine.valid = false;
         return parsedLine;
     }
 
     // read line content into struct
-    parsedLine.bytecount = lineByteArray.at(0);
-    parsedLine.address = lineByteArray.mid(1,2).toShort();
-    parsedLine.entyType = lineByteArray.at(3);
-    parsedLine.data = lineByteArray.mid(4, 4 + parsedLine.bytecount/2);
+    parsedLine.bytecount = (quint8)lineByteArray.at(0);
+    parsedLine.address = (quint8)lineByteArray.at(1) << 8;
+    parsedLine.address |= (quint8)lineByteArray.at(2);
+    parsedLine.entyType = (quint8)lineByteArray.at(3);
+    parsedLine.data = lineByteArray.mid(4, 4 + parsedLine.bytecount);
     parsedLine.checksum = lineByteArray.back();
 
     // check if line fits with checksum
